8cbfb1764dd9a7412cc35491273f3cae4126b6b0
Aldi <aprialdinur@gmail.com>
2024-11-24 22:59:49 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Teks di tengah layar */}
+      <Text style={styles.centerText}>Aprialdi Nurhidayat - 223510662</Text>
+
+      {/* Kotak dengan Flexbox */}
+      <View style={styles.boxContainer}>
+        <View style={[styles.box, styles.boxLeft]} />
+        <View style={[styles.box, styles.boxRight]} />
+      </View>
     </View>
   );
 }
@@ -13,8 +19,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#D3D3D3', // Latar belakang abu-abu terang
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerText: {
+    fontSize: 24,
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  boxContainer: {
+    flexDirection: 'row', // Menyusun kotak secara horizontal
+    justifyContent: 'space-between', // Jarak merata antar kotak
+    width: '80%', // Atur lebar total untuk kotak
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  boxLeft: {
+    backgroundColor: 'red', // Warna bebas untuk kotak 1
+  },
+  boxRight: {
+    backgroundColor: 'green', // Warna bebas untuk kotak 2
   },
 });
